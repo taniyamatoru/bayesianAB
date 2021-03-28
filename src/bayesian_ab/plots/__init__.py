4b78f69179a29e28defe4008a65f36f72453615c
sakuv2 <loots2438@gmail.com>
2021-03-28 15:19:03 +0900

    norm

--- src/bayesian_ab/plots/__init__.py
+++ src/bayesian_ab/plots/__init__.py
@@ -1,3 +1,3 @@
+from .animation import gen_posterior_animation  # noqa
 from .distribution import fig_1d  # noqa
 from .histogram import histogram, histogramAB  # noqa
-from .animation import gen_posterior_animation  # noqa
